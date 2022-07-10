5a92b72a29cce7e8699f464320456a943d366edd
SUBHODEEP MOITRA <64082425+subhodeepmoitra@users.noreply.github.com>
2022-07-10 12:39:40 +0530

    pcr login linked to pcr home

--- src/pcr_management_system_pcrms/pcr_login.java
+++ src/pcr_management_system_pcrms/pcr_login.java
@@ -4,16 +4,23 @@
  */
 package pcr_management_system_pcrms;
 import pcr_registration.pcr_registration;
+import pcr_modules.pcr_home;
+import java.sql.*;
+import javax.swing.JOptionPane;
 /**
  *
  * @author subho
  */
 public class pcr_login extends javax.swing.JFrame {
+        Connection con = null;
+        PreparedStatement pst = null;
+        ResultSet rs = null;
 
     /**
      * Creates new form pcr_login
      */
     public pcr_login() {
+
         initComponents();
     }
 
@@ -31,11 +38,11 @@ public class pcr_login extends javax.swing.JFrame {
         jLabel3 = new javax.swing.JLabel();
         jLabel4 = new javax.swing.JLabel();
         jLabel5 = new javax.swing.JLabel();
-        jTextField1 = new javax.swing.JTextField();
+        pcr_number = new javax.swing.JTextField();
         jLabel6 = new javax.swing.JLabel();
-        jPasswordField1 = new javax.swing.JPasswordField();
-        jButton1 = new javax.swing.JButton();
-        jButton2 = new javax.swing.JButton();
+        password = new javax.swing.JPasswordField();
+        login = new javax.swing.JButton();
+        register_new_pcr = new javax.swing.JButton();
 
         jLabel2.setText("jLabel2");
 
@@ -52,26 +59,26 @@ public class pcr_login extends javax.swing.JFrame {
         jLabel5.setFont(new java.awt.Font("Segoe UI", 1, 18)); // NOI18N
         jLabel5.setText("PCR Number");
 
-        jTextField1.addActionListener(new java.awt.event.ActionListener() {
+        pcr_number.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jTextField1ActionPerformed(evt);
+                pcr_numberActionPerformed(evt);
             }
         });
 
         jLabel6.setFont(new java.awt.Font("Segoe UI", 1, 18)); // NOI18N
         jLabel6.setText("Password");
 
-        jButton1.setText("Login");
-        jButton1.addActionListener(new java.awt.event.ActionListener() {
+        login.setText("Login");
+        login.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jButton1ActionPerformed(evt);
+                loginActionPerformed(evt);
             }
         });
 
-        jButton2.setText("Register new PCR");
-        jButton2.addActionListener(new java.awt.event.ActionListener() {
+        register_new_pcr.setText("Register new PCR");
+        register_new_pcr.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jButton2ActionPerformed(evt);
+                register_new_pcrActionPerformed(evt);
             }
         });
 
@@ -96,11 +103,11 @@ public class pcr_login extends javax.swing.JFrame {
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                         .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
                             .addGroup(layout.createSequentialGroup()
-                                .addComponent(jButton2, javax.swing.GroupLayout.PREFERRED_SIZE, 167, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                .addComponent(register_new_pcr, javax.swing.GroupLayout.PREFERRED_SIZE, 167, javax.swing.GroupLayout.PREFERRED_SIZE)
                                 .addGap(18, 18, 18)
-                                .addComponent(jButton1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
-                            .addComponent(jPasswordField1)
-                            .addComponent(jTextField1))
+                                .addComponent(login, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                            .addComponent(password)
+                            .addComponent(pcr_number))
                         .addGap(65, 65, 65))))
         );
         layout.setVerticalGroup(
@@ -113,38 +120,58 @@ public class pcr_login extends javax.swing.JFrame {
                     .addComponent(jLabel4, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
                 .addGap(18, 18, 18)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addComponent(jTextField1)
+                    .addComponent(pcr_number)
                     .addComponent(jLabel5, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
                 .addGap(26, 26, 26)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addGroup(layout.createSequentialGroup()
                         .addGap(4, 4, 4)
-                        .addComponent(jPasswordField1, javax.swing.GroupLayout.PREFERRED_SIZE, 25, javax.swing.GroupLayout.PREFERRED_SIZE))
+                        .addComponent(password, javax.swing.GroupLayout.PREFERRED_SIZE, 25, javax.swing.GroupLayout.PREFERRED_SIZE))
                     .addComponent(jLabel6))
                 .addGap(43, 43, 43)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(jButton1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                    .addComponent(jButton2))
+                    .addComponent(login, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                    .addComponent(register_new_pcr))
                 .addGap(72, 72, 72))
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
-    private void jTextField1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jTextField1ActionPerformed
+    private void pcr_numberActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_pcr_numberActionPerformed
         // TODO add your handling code here:
-    }//GEN-LAST:event_jTextField1ActionPerformed
+    }//GEN-LAST:event_pcr_numberActionPerformed
 
-    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+    private void loginActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_loginActionPerformed
         // TODO add your handling code here:
-    }//GEN-LAST:event_jButton1ActionPerformed
+        try{
+            String query = "SELECT * FROM `pcr_registration` WHERE pcr_number = ? AND password = ?";
+            con = DriverManager.getConnection("jdbc:mysql://localhost:3306/pcrms","root","");
+            pst = con.prepareStatement(query);
+            pst.setString(1, pcr_number.getText());
+            pst.setString(2, password.getText());
+            rs = pst.executeQuery();
+            if (rs.next()){
+                JOptionPane.showMessageDialog(null, "Login Success");
+                pcr_home pcrhome = new pcr_home();
+                pcrhome.setVisible(true);
+                this.dispose();
+            }
+            else{
+                JOptionPane.showMessageDialog(null, "Incorrect Credentials");
+            }
+        }
+        catch (Exception ex){
+            JOptionPane.showMessageDialog(null, "Preblems in Login");
+        }
+    }//GEN-LAST:event_loginActionPerformed
 
-    private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
+    private void register_new_pcrActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_register_new_pcrActionPerformed
         // TODO add your handling code here:
         pcr_registration pcr_reg = new pcr_registration();
         pcr_reg.setVisible(true);
         this.dispose();
-    }//GEN-LAST:event_jButton2ActionPerformed
+    }//GEN-LAST:event_register_new_pcrActionPerformed
 
     /**
      * @param args the command line arguments
@@ -182,15 +209,15 @@ public class pcr_login extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JButton jButton1;
-    private javax.swing.JButton jButton2;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
     private javax.swing.JLabel jLabel3;
     private javax.swing.JLabel jLabel4;
     private javax.swing.JLabel jLabel5;
     private javax.swing.JLabel jLabel6;
-    private javax.swing.JPasswordField jPasswordField1;
-    private javax.swing.JTextField jTextField1;
+    private javax.swing.JButton login;
+    private javax.swing.JPasswordField password;
+    private javax.swing.JTextField pcr_number;
+    private javax.swing.JButton register_new_pcr;
     // End of variables declaration//GEN-END:variables
 }
